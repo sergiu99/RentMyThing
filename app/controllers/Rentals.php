556e3bde3598ef63da209eb52d6fe0f90b330644
sergiu99 <sergiu.ifimov@gmail.com>
2017-11-28 16:08:01 -0500

    1

--- app/controllers/Rentals.php
+++ app/controllers/Rentals.php
@@ -18,6 +18,33 @@ class Rentals extends Controller{
 
     }
     
+    function createComment(){
+        if(isset($_POST['rentalId']) && isset($_POST['content']) && isset($_POST['rating'])){
+
+        $userId =  $_SESSION['userID'];
+        $newItem = $this->model('Comment');
+
+        $newItem->rental_id = $_POST['rentalId'];
+        $newItem->content = $_POST['content'];
+        $newItem->rating = $_POST['rating'];
+        $newItem->poster_status = $userId;
+        $newId = $newItem->insert();
+        header("location:/Rentals");
+
+        } 
+        else if(isset($_POST['rentalId']))
+        { 
+        $aItem = $this->model('Rental');
+        $aItem = $aItem->find($_POST['rentalId']);
+                $this->view('Rentals/commentForm',['RentalObject'=>$aItem]);
+
+        } else {
+            var_dump($_POST);
+             //header("location:/Rentals");
+        }
+    }
+
+
     function Action(){
 var_dump($_POST);
 
@@ -35,23 +62,47 @@ var_dump($_POST);
 
         $newNotification = $this->model('Notification');
         $newNotification->user_id = $aItem->user_id;
+        $newNotification->redirect = "/Rentals";
         $contentt = 'Someone has declined your rental request.';
         $newNotification->content = $contentt;
-        $newNotification->redirect = "/Rentals";
         $newNotification = $newNotification->insert();
         }
+
 
         if($_POST['actionType'] == 'complete'){
         $aItem = $this->model('Rental');
         $aItem = $aItem->find($id);
-        $aItem->status = 'completed';
-        $aItem->update();
-
+        $statuss = $aItem->status;
         $newNotification = $this->model('Notification');
         $newNotification->user_id = $aItem->user_id;
+        $newNotification->redirect = "/Rentals";
+
+
+
+        if($statuss == 'pending'){
+        $aItem->status = 'completed';
+        $contentt = 'One of your rentals has been terminated before starting';
+        $newNotification->content = $contentt;
+
+        } else if($statuss == 'accepted'){
+             $userId =  $_SESSION['userID'];
+         $aItem->status = 'reqcompleted' . $userId;
+        $contentt = 'One of your rentals needs your approval to be completed';
+        $newNotification->content = $contentt;
+
+        } else {
+        $aItem->status = 'completed';
         $contentt = 'One of your rentals has been completed';
         $newNotification->content = $contentt;
-        $newNotification->redirect = "/Rentals";
+        }
+        
+        if($statuss != 'pending'){
+           // Send request to rental requester for comment
+        }
+
+        $aItem->update();
+
+        
         $newNotification = $newNotification->insert();
         }
 
---
+++ app/views/Rentals/commentForm.php
@@ -0,0 +1,40 @@
+<?php  include($_SERVER['DOCUMENT_ROOT'] . '/app/views/top.php'); ?>
+
+<div class="container">
+<br>
+<br>
+
+<form method="post" action="/Rentals/createComment" class="form-horizontal" enctype="multipart/form-data">
+
+<?php
+
+$rental = $data['RentalObject'];
+
+echo "<h2>Leave a comment for the rental</h2>";
+        echo "<input id='rentalId' name='rentalId' type='hidden' value='$rental->id'/>";
+       // echo "<input id='actionType' name='actionType' type='hidden' value='complete'/>";
+?>
+
+	
+	<div class="form-group">
+	<label for="description">Content</label>
+	<textarea rows="4" type="text" class="form-control" required="true" name="content" id="content"></textarea>
+	</div>
+	<div class="form-group  row col-sm-3">
+<label for="urgency">Rating out of 5</label> <br>
+<select class="form-control" name="rating" required="true" id="rating">
+  <option value="5">5</option>
+  <option value="4">4</option>
+  <option value="3">3</option>
+  <option value="2">2</option>
+  <option value="1">1</option>
+</select>
+</div>
+	<div class="form-group">
+	<input type="submit" class="btn btn-default" name="action" value="Create comment />
+	</div>
+</form>
+
+</div>
+</body>
+</html>
